7e4fc0077719129fcb53cc9920117c11040a9589
MothMalone <nam28042004@gmail.com>
2025-10-02 18:29:35 +0700

    Decision_Table test for calculateTotalCost

---
+++ tests/decision_table.cpp
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <cmath>
+#include "../src/funds_calculator.h"
+#include "../src/gpu_model.h"
+
+const double EPSILON = 0.001;
+
+// 4.1. calculateTotalCost Decision Table Tests
+TEST(CalculateTotalCostTest, DecisionTableTests) {
+
+    // TC1: Invalid input <- hourlyRate > 0, instanceCount > 0, runningHours < 0, any dailyStorageCost (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(1.0, 5, -50, 0.5);
+
+    // TC2: Invalid input <- hourlyRate > 0, instanceCount > 0, runningHours = 0, dailyStorageCost < 0 (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(1.0, 5, 0, -0.5);
+
+    // TC3: Result = 0 <- hourlyRate > 0, instanceCount > 0, runningHours = 0, dailyStorageCost = 0
+    EXPECT_NEAR(0.0, calculateTotalCost(1.0, 5, 0, 0.0), EPSILON);
+
+    // TC4: Result = 0 <- hourlyRate > 0, instanceCount > 0, runningHours = 0, dailyStorageCost > 0
+    EXPECT_NEAR(0.0, calculateTotalCost(1.0, 5, 0, 0.5), EPSILON);
+
+    // TC5: Normal with running hours <- hourlyRate > 0, instanceCount > 0, runningHours > 0, dailyStorageCost > 0
+    EXPECT_NEAR(257.5, calculateTotalCost(1.0, 5, 50, 0.5), EPSILON);
+
+
+    // TC6: Only runtime <- hourlyRate > 0, instanceCount > 0, runningHours > 0, dailyStorageCost = 0
+    EXPECT_NEAR(250.0, calculateTotalCost(1.0, 5, 50, 0.0), EPSILON);
+
+
+    // TC7: Invalid input <- hourlyRate > 0, instanceCount > 0, runningHours > 0, dailyStorageCost < 0 (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(1.0, 5, 50, -0.5);
+    
+    // TC8: Invalid input <- hourlyRate > 0, instanceCount <= 0, any runningHours , any dailyStorageCost (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(1.0, 0, 50, 0.5);
+    
+    // TC9: Invalid input <- hourlyRate < 0, any instanceCount, any runningHours, any dailyStorageCost (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(-1.0, 0, 50, 0.5);
+    
+    // TC10: Invalid input <- hourlyRate = 0, instanceCount <= 0, any runningHours, any dailyStorageCost (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(0.0, -5, 50, 0.5);
+
+
+    // TC11: Only Storage <- hourlyRate = 0, instanceCount > 0, runningHours > 0, dailyStorageCost > 0
+    EXPECT_NEAR(7.5, calculateTotalCost(0.0, 5, 50, 0.5), EPSILON);
+    
+    // TC12: Result = 0 <- hourlyRate = 0, instanceCount > 0, runningHours > 0, dailyStorageCost = 0
+    EXPECT_NEAR(0.0, calculateTotalCost(0.0, 5, 50, 0.0), EPSILON);
+
+
+    // TC13: Invalid input <- hourlyRate = 0, instanceCount > 0, runningHours > 0, dailyStorageCost < 0 (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(0.0, 5, 50, -0.5);
+
+
+    // TC14: Result = 0 <- hourlyRate = 0, instanceCount > 0, runningHours = 0, dailyStorageCost > 0
+    EXPECT_NEAR(0.0, calculateTotalCost(0.0, 5, 0, 0.5), EPSILON);
+    
+    // TC15: Result = 0 <- hourlyRate = 0, instanceCount > 0, runningHours = 0, dailyStorageCost = 0
+    EXPECT_NEAR(0.0, calculateTotalCost(0.0, 5, 0, 0.0), EPSILON);
+
+
+    // TC16: Invalid input <- hourlyRate = 0, instanceCount > 0, runningHours = 0, dailyStorageCost < 0 (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(0.0, 5, 0, -0.5);
+
+    // TC17: Invalid input <- hourlyRate = 0, instanceCount > 0, runningHours < 0, any dailyStorageCost (not handled in function on unit level)
+    // EXPECT("Invalid", calculateTotalCost(0.0, 5, -50, 0.5);
+}
+
+
+
